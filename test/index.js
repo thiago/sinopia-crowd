233cbbade3e62cedde2d8838ce8031fb1795058f
Thiago Rodrigues <thiagorsouz@gmail.com>
2015-11-15 22:45:27 -0200

    initial connection with crowd

--- test/index.js
+++ test/index.js
@@ -1,10 +1,45 @@
 'use strict';
 
 var assert = require('assert');
-var sinopiaCrowd = require('../lib');
+var plugin = require('../lib');
+var nock = require('nock');
+var nocks = nock.load(__dirname + '/fixtures.json');
+var stuff = {
+  config: {
+    self_path: __dirname + '/config'
+  },
+  logger: {}
+};
 
-describe('sinopia-crowd', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+var config = {
+  base: 'http://mycrowdserver.com/',
+  name: 'sinopia-crowd',
+  password: 'sinopia-crowd'
+};
+
+describe('sinopia-crowd', function() {
+
+  it('should have plugin interface', function() {
+    assert.equal(typeof plugin, 'function');
+    var p = plugin(config, stuff);
+    assert.equal(typeof p.authenticate, 'function');
+  })
+
+  it('should not authenticate random user', function(cb) {
+    var p = plugin(config, stuff);
+    p.authenticate('blah', 'blah', function(err, groups) {
+      assert(!err)
+      assert(!groups)
+      cb()
+    })
+  })
+
+  it('should add user', function(cb) {
+    var p = plugin(config, stuff)
+    p.adduser('foo1', 'bar1', function(err, ok) {
+      assert(!err)
+      assert(ok)
+      cb()
+    })
   });
-});
+})
